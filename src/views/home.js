541e30f030dfc14f74821f2d4837d0ad56d1b59b
gilberto-009199 <gilberto.tec@vivaldi.net>
2020-06-24 14:45:15 -0300

    Adicionando lista dos itens do torrent e pensando em me livrar do primereact

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,9 @@
 import WebTorrent from 'webtorrent';
-
 import React,{ Component } from 'react';
 import {PanelMenu} from 'primereact/panelmenu';
 import {DataTable} from 'primereact/datatable';
+import {Message} from 'primereact/message';
+import {TreeTable} from 'primereact/treetable';
 import {ProgressBar} from 'primereact/progressbar';
 import {Column} from 'primereact/column';
 import {Sidebar} from 'primereact/sidebar';
@@ -22,16 +23,46 @@ class home2 extends Component {
         this.state.visible = false;
         this.state.torrents = {};
         this.state.torrents.items = [
-                                        {
+                {
 
-                                            name:' v de vingança',
-                                            complite:100,
-                                            download:200,
-                                            upload:0,
-                                            torrentId:'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
-                                        }
+                    name:' v de vingança',
+                    complite:100,
+                    download:200,
+                    upload:0,
+                    torrentId:'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+                },
+                {
+                    name:' xena',
+                    complite:100,
+                    download:200,
+                    upload:0,
+                    torrentId:'magnet:?xt=urn:btih:ae8534514d0c0879eda301114ac62f45c346306e&dn=STHPRKT10EP08.mp4&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
+                }
+
                                     ];
-        this.state.menu = { items:[           {                   label:'File',                   icon:'pi pi-fw pi-file',                   items:[                      {                         label:'New',                         icon:'pi pi-fw pi-plus',                         items:[                            {                               label:'Bookmark',                               icon:'pi pi-fw pi-bookmark'                            },                            {                               label:'Video',                               icon:'pi pi-fw pi-video'                            },]                      }                   ]                }             ]        };    
+        this.state.menu = { items:[
+                                    {
+                                        label:'File',
+                                        icon:'pi pi-fw pi-file',
+                                        items:[
+                                           {
+                                               label:'New',
+                                               icon:'pi pi-fw pi-plus',
+                                               items:[
+                                                 {
+                                                     label:'Bookmark',
+                                                     icon:'pi pi-fw pi-bookmark'
+                                                 },
+                                                 {
+                                                     label:'Video',
+                                                     icon:'pi pi-fw pi-video'
+                                                  }
+                                               ]
+                                            }
+                                        ]
+                                      }
+                                    ]
+                           };    
         
         this.rowExpansionTemplate = this.rowExpansionTemplate.bind(this);
         this.client = new WebTorrent(); 
@@ -45,15 +76,48 @@ class home2 extends Component {
     rowExpansionTemplate(data){
         console.log("Data!",data);
         
-        
-        this.state.view = document.querySelector('video#video-view');
-        var file = data.torrent.files.find(function (file) { return file.name.endsWith('.mp4')});
-        
-        file.renderTo(this.state.view)
 
-        return <div>
-                    lista de arquivos
-              </div>;
+
+        this.state.view = document.querySelector('video#video-view');
+        
+
+        // Paraliza o progresso da aplicação caso o torrent selecioando n esteja pronto
+        if(!data.torrent){
+            
+            return(
+                    <>
+                        <Message severity="info" text="Torrente ainda em progresso!"></Message>
+                    </>
+                  )
+                
+        }
+        
+        
+        /*var file = data.torrent.files.find(function (file) { 
+
+            console.log(file);
+            return file.name.endsWith('.mp4')
+
+        });
+        //var file = data.torrent.files.find(function (file) { return file.name.endsWith('.rmvb')});
+        file.renderTo(this.state.view)/**/
+        
+        console.log(data.torrent.files)
+        var files = data.torrent.files;
+        return (
+            <>
+                { 
+                    files.map(function(item,index){
+                        return(
+                            <>
+                                <label>{item.name}</label><br/>
+                            </>
+                        )
+                    })
+                }
+            </>
+        );
+
     }
     progressbarTemplate(data){
         return( 
@@ -65,7 +129,7 @@ class home2 extends Component {
             <div>
                 <div className="p-grid">
                      
-                     <Sidebar fullScreen={true} visible={this.state.visible}>
+                     <Sidebar fullScreen={true} onHide={() => this.setState({visible:false})} visible={this.state.visible}>
                         
                         <h1>hellow</h1>
                     </Sidebar>
